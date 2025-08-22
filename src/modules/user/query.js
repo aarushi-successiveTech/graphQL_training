9519df6fedaa98f1aca6a00bb9b47f5387b3d1a2
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-08-22 14:24:26 +0530

    assignment4 completed

--- src/modules/user/query.js
+++ src/modules/user/query.js
@@ -4,10 +4,12 @@ import { User } from "./model/userModel.js";
 export const userQueryResolvers = {
 
     displayUsers: async(_, __, context) => {
+        console.log('Resolver context:', context);
+        console.log('context: ', context.user); 
+        
         if(!context.user){
             throw new Error('Not authenticated');
         }
-        console.log(context.user)
         authRole(context.user, ['admin']); 
         const allUsers = await User.find();
         return allUsers; 
